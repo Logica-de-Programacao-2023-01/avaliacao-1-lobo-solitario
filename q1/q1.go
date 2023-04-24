38c13bb5d98c29de553b3354a8e7076ba670422f
PedroArthurJardim <126472164+PedroArthurJardim@users.noreply.github.com>
2023-04-24 08:45:05 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -2,17 +2,23 @@ package q1
 
 import (
 	"errors"
-	"fmt"
 )
+
 func DivideWatermelon(weight int) (bool, error) {
 	if weight <= 0 {
 		return false, errors.New("Peso inválido")
 	}
-	if weight%2 == 0 {
-		metade := weight / 2
-		if metade%2 == 0 {
-			fmt.Print("É possível: ", true)
-		}
+	par := false
+	if weight == 2{
+		return false, nil
 	}
+	if weight%2 == 0{
+		par = true
+		return true, nil
+	}
+	if par{
+		return false, nil
+	}
+	
 	return false, nil
 }
