2a98379ec6124f842b23cf360b480fa30351e5f6
PedroArthurJardim <126472164+PedroArthurJardim@users.noreply.github.com>
2023-04-24 10:59:07 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -1,26 +1,30 @@
 package q4
 
-import "errors"
+import "fmt"
 
 func ClassifyPrices(prices []int) (int, error) {
-	if prices == nil {
-		return 0, errors.New("Valor inválido")
+	if len(prices) == 0{
+		return 0, fmt.Errorf("lista inválida")
 	}
-	for i := 1; i > len(prices); i++ {
-		if prices[i] > prices[i-1] {
+	if len(prices) == 1 {
+		return 3, nil
+	}
+	var crescente bool = true
+	var decrescente bool = true
+	for i:= 1; i < len(prices); i++{
+		if prices[i] < prices[i-1]{
+			crescente = false
+		} else if prices[i] > prices[i-1] {
+			decrescente = false
 		}
+		
+	}
+	if crescente == true{
 		return 1, nil
-	}
-	for i := 1; i > len(prices); i++ {
-		if prices[i] < prices[i-1] {
-		}
+	} else if decrescente == true{
 		return 2, nil
-	}
-	for i := 1; i > len(prices); i++ {
-		if prices[i] == prices[i-1] {
-		}
+	}else{
 		return 3, nil
 	}
 	return 0, nil
 }
-
